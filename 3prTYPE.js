db78cededc7a52227ae4b6af95155a9a8b3ef01c
developersnt <developer.snt@outlook.com>
2024-11-16 12:36:34 +0530

    Add javascript practical

---
+++ 3prTYPE.js
@@ -0,0 +1,8 @@
+    let name  ="hello";
+    let age = 12
+    let status = true
+
+    console.log(typeof age);
+console.log(typeof undefined)
+   let number
+    console.log(number + number +number +2+3);
---
+++ account.js
@@ -0,0 +1,8 @@
+const accunt_Id = 123456
+let accountemail = "demo123@gmail.com"
+var password = "1234";
+accountName= "abc"
+let accountstatusa
+
+// console.log(accunt_Id);
+console.table([accunt_Id,accountemail,password,accountName,accountstatusa])
